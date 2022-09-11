c1efe6770d64af9d0c47166d30c215a4282b0086
Phillip Trudeau-Tavara <pmttavara@protonmail.com>
2022-09-11 04:26:56 -0400

    Threaded example work

--- tools/flint.h
+++ tools/flint.h
@@ -32,8 +32,9 @@ Optional Helper APIs:
 Example Threaded Implementation:
     enum { RING_BUFFER_SIZE = 65536 };
     struct Event {
-        double when;
+        uintptr_t is_end; // same size as const char* to avoid padding bytes
         const char *name;
+        double when;
     };
     struct RegisteredThread {
         uint32_t pid;
@@ -46,10 +47,12 @@ Example Threaded Implementation:
         Event events[RING_BUFFER_SIZE];
     };
     struct ProfileContext {
-        cnd_t recording;
+        Semaphore recording;
+        Semaphore any_thread_has_written;
         bool never_drop_events;
         Mutex mutex; {
-            RegisteredThread *registered_threads;
+            RegisteredThread **registered_threads;
+            int n_threads;
             SpallContext ctx;
         }
     };
@@ -85,30 +88,42 @@ Example Threaded Implementation:
     int output_thread(void *userdata) {
         ProfileContext *profile = (ProfileContext *)userdata;
         while (true) {
-            cond_wait(profile->recording);
-            output_profile();
-            Sleep(1);
+            wait_for_semaphore_forever(profile->recording);
+            wait_for_semaphore_forever(profile->any_thread_has_written);
+            output_profile(profile);
         }
     }
 
-    EventID trace_begin(ProfileContext *profile, RegisteredThread *thread, const char *name) {
+    // note: /Ob1 is how you would get FORCE_INLINE to work on msvc in debug mode
+    inline void trace_begin(ProfileContext *profile, RegisteredThread *thread, const char *name) {
+        if (UNLIKELY(profile->never_drop_events & profile->recording)) { // note: bitwise and could reduce branch predict slots?
+            if (UNLIKELY(thread.read_head <= thread.write_head - (RING_BUFFER_SIZE - 1))) {
+                output_profile(profile);
+            }
+        }
+        Event *event = &thread->events[thread->write_head & (RING_BUFFER_SIZE - 1)];
 #ifdef DYNAMIC_STRINGS
-        SPALL_FREE(thread->events[thread->write_head & (RING_BUFFER_SIZE - 1)].name, thread->allocator_userdata);
+        if (LIKELY(event->name)) SPALL_FREE(event->name, thread->allocator_userdata);
         name = SPALL_STRDUP(name, thread->allocator_userdata);
 #endif
-        thread->events[thread->write_head & (RING_BUFFER_SIZE - 1)] = { false, thread->thread_depth++, name, __rdtsc() };
+        *event = { false, thread->thread_depth++, name, __rdtsc() };
         ++thread->write_head; // atomic
+        signal_semaphore(profile->any_thread_has_written);
     }
-    void trace_end(ProfileContext *profile, RegisteredThread *thread, EventID id) {
+    inline void trace_end(ProfileContext *profile, RegisteredThread *thread, EventID id) {
+        Event *event = &thread->events[thread->write_head & (RING_BUFFER_SIZE - 1)];
         thread->events[thread->write_head & (RING_BUFFER_SIZE - 1)] = { true, id, thread->events[thread->write_head & (RING_BUFFER_SIZE - 1)].name, __rdtsc() };
         ++thread->write_head; // atomic
+        signal_semaphore(profile->any_thread_has_written);
     }
 
     RegisteredThread *thread_init(ProfileContext *profile, u32 pid, u32 tid, u8 ring_buffer_size_power) {
-        
+        // handle = CreateSemaphoreA()
+        RegisteredThread *result = array_calloc_and_append(&profile->registered_threads, &profile->n_threads);
     }
-    void thread_quit(RegisteredThread *) {
-        
+    void thread_quit(RegisteredThread *thread) {
+        output_profile(profile);
+        array_remove_unordered_and_free(&profile->registered_threads, thread);
     }
 */
 
